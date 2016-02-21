30d694b9d9dd7d289293d8107749e789361b87c7
Krzysztof Kaczkowski <krzysztof.kaczkowski@architekt-it.pl>
2016-02-21 17:44:34 +0100

    Correct driver to read ultrasonic.

--- robot/sonar/asdic.py
+++ robot/sonar/asdic.py
@@ -11,43 +11,58 @@ GPIO.setmode(GPIO.BCM)
 
 class ASDIC:
     
-    def clean(self):
-        GPIO.cleanup()
-    
-    def ping(self):
-        """
-        Return distance in cm
-        """
-        
-        print("Distance measurement in progress...")
+    def __init__(self):
         GPIO.setup(TRIG, GPIO.OUT)
         GPIO.setup(ECHO, GPIO.IN)
         
         GPIO.output(TRIG, False)
         print("Waiting for sensor to settle")
-        time.sleep(2)
-        
+        time.sleep(1)
+
+
+    def clean(self):
+        GPIO.cleanup()
+
+
+    def ping(self):
+        """
+        HS-SR04
+        Return distance in cm
+        """
+
         GPIO.output(TRIG, True)
         time.sleep(0.00001)
         GPIO.output(TRIG, False)
         
+        zero = time.time()
+        pulse_start = time.time()
         while GPIO.input(ECHO) == 0:
             pulse_start = time.time()
+            if pulse_start - zero > 0.1:
+                return -1
             
+        zero = time.time()
+        pulse_end = time.time()
         while GPIO.input(ECHO) == 1:
             pulse_end = time.time()
+            if pulse_end - zero > 0.1:
+                return -1
             
         pulse_duration = pulse_end - pulse_start
         distance = pulse_duration * 17150
         
-        GPIO.cleanup()
+        if distance < 2:
+            return -1
         
-        return round(distance, 2)
+        return round(distance, 1)
         
 
 if __name__ == "__main__":
     asdic = ASDIC()
     atexit.register(GPIO.cleanup)
-    print(asdic.ping())
+    while 1:
+        p = asdic.ping()
+        print(asdic.ping())
+        time.sleep(0.01)
         
         
