808a08d54f53f12df79df87b190546b4bc0bdbf7
kkaczkowski <krzysztof.kaczkowski@architekt-it.pl>
2016-03-13 00:50:47 +0100

    Add start robot.

---
+++ start.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import time
+from robot.track import RobotEngine
+from robot.sonar.asdic import ASDIC
+from robot.motors.motor_servo import PWM
+
+robot = RobotEngine()
+asdic = ASDIC()
+
+robot.speed_motor(30)
+
+while 1:
+    distance = asdic.ping()
+    if distance < 30:
+        robot.left(30)
+    else:
+        robot.speed_motor(30)
+    time.sleep(0.01)
