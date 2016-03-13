c41e1503d0bcfd13451dbc4d752cd03eaa2a808d
kkaczkowski <krzysztof.kaczkowski@architekt-it.pl>
2016-03-13 01:28:07 +0100

    Add advanced speed control.

--- start.py
+++ start.py
@@ -8,13 +8,18 @@ from robot.motors.motor_servo import PWM
 robot = RobotEngine()
 asdic = ASDIC()
 
-robot.speed_motor(30)
+MAX_SPEED = 40
+
 
 while 1:
     distance = asdic.ping()
     print(distance)
-    if distance < 30:
+    if distance < 10:
+        robot.speed_motor(-MAX_SPEED)
+    if distance < 20:
+        robot.stop()
+    if distance < 40:
         robot.turn(30)
     else:
-        robot.speed_motor(30)
+        robot.speed_motor(MAX_SPEED)
     time.sleep(0.01)
